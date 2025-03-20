b515de7e1cc4a6a4dab08a1045dc77f4d2cfd3c1
LeBelin <andy.belin64@gmail.com>
2025-03-20 15:30:54 +0100

    init

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+    Route::view('Clients', 'clients')
+    ->middleware(['auth', 'verified'])
+    ->name('clients');
+
+    Route::view('Prospects', 'prospects')
+    ->middleware(['auth', 'verified'])
+    ->name('prospects');
+
+    Route::view('Commerciaux', 'commerciaux')
+    ->middleware(['auth', 'verified'])
+    ->name('commerciaux');
+
+    Route::view('Produits', 'produits')
+    ->middleware(['auth', 'verified'])
+    ->name('produits');
+
+    Route::view('Commande', 'commande')
+    ->middleware(['auth', 'verified'])
+    ->name('commande');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
