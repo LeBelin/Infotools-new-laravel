20d556b8d774e26683fd890386e42d9eddad435e
Le Belin <117570222+LeBelin@users.noreply.github.com>
2025-03-26 22:08:25 +0100

    change size logo authentification

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,2 +1,2 @@
-<!-- Logo login et register -->
-<img src="{{ asset('logo.png') }}" alt="Mon Logo" class="w-28 h-auto mx-auto">
+<!-- Logo login et register, pour la taille sa se passe dans simple.blade.php dans le span-->
+<img src="{{ asset('logo.png') }}" alt="Mon Logo" class="w-auto h-auto mx-auto">
