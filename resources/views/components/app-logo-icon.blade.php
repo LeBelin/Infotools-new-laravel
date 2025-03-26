3c4e096690bca92e8fdb9b65163f8479c281d38b
Le Belin <117570222+LeBelin@users.noreply.github.com>
2025-03-26 22:55:29 +0100

    logo size

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,2 +1,1 @@
-<!-- Logo login et register, pour la taille sa se passe dans simple.blade.php dans le span-->
-<img src="{{ asset('logo.png') }}" alt="Mon Logo" class="w-auto h-auto mx-auto">
+<img src="{{ asset('logo.png') }}" alt="Mon Logo" class="max-w-full max-h-full">
