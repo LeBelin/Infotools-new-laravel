148021669f1c4a892147a2c4cbb23bc91bcc9197
LeBelin <andy.belin64@gmail.com>
2025-03-27 14:54:04 +0100

    translate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,18 @@
-
-        
-        <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Infotools</title>
+        <title>Infotools</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-    <!-- Lien vers ton fichier CSS -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
+        <!-- Lien vers ton fichier CSS -->
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    </head>
 <body>
     <!-- Header avec navigation -->
     <header class="header">
