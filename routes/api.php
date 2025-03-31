1de7f1ff34f6bdbd6d52222f09462872ae8e0f7c
Le Belin <117570222+LeBelin@users.noreply.github.com>
2025-03-31 19:14:45 +0200

    api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// API routes for rendez vous
+//Route::get('/rendez-vous', 'App\Http\Controllers\RendezVousController@index');
