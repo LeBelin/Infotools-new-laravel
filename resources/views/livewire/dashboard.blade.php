a26e6ed89a081a1a385bbaa53105a2152172d462
Le Belin <117570222+LeBelin@users.noreply.github.com>
2025-04-10 22:54:58 +0200

    update dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -13,9 +13,9 @@
     <section class="flex items-center justify-between p-4">
         <h2 class="text-lg font-semibold">Statistiques</h2>
     </section>
-    <div class="flex flex-wrap gap-4">
+    <div class="flex flex-wrap">
         <!-- Clients -->
-        <div class="flex flex-col items-center p-6 bg-white rounded-xl shadow-md hover:shadow-lg transition duration-300 ease-in-out w-full sm:w-1/2 md:w-1/4">
+        <div class="flex flex-col items-center p-6 bg-white ease-in-out w-full sm:w-1/2 md:w-1/4">
             <div class="stat-icon bg-blue-100 p-4 rounded-full mb-4">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon h-7 w-7 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a4 4 0 00-4-4h-1M9 20H4v-2a4 4 0 014-4h1m4-4a4 4 0 110-8 4 4 0 010 8zm6 4a4 4 0 100-8 4 4 0 000 8z" />
@@ -26,7 +26,7 @@
         </div>
 
         <!-- Produits -->
-        <div class="flex flex-col items-center p-6 bg-white rounded-xl shadow-md hover:shadow-lg transition duration-300 ease-in-out w-full sm:w-1/2 md:w-1/4">
+        <div class="flex flex-col items-center p-6 bg-white ease-in-out w-full sm:w-1/2 md:w-1/4">
             <div class="stat-icon bg-green-100 p-4 rounded-full mb-4">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon h-7 w-7 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12v7a2 2 0 01-2 2H6a2 2 0 01-2-2v-7m16 0L12 3 4 12m16 0H4" />
@@ -37,7 +37,7 @@
         </div>
 
         <!-- Commandes -->
-        <div class="flex flex-col items-center p-6 bg-white rounded-xl shadow-md hover:shadow-lg transition duration-300 ease-in-out w-full sm:w-1/2 md:w-1/4">
+        <div class="flex flex-col items-center p-6 bg-white ease-in-out w-full sm:w-1/2 md:w-1/4">
             <div class="stat-icon bg-yellow-100 p-4 rounded-full mb-4">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon h-7 w-7 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h14l-1.35 6.78a2 2 0 01-1.97 1.72H7a2 2 0 01-2-2V6h16m-5 12a1 1 0 100 2 1 1 0 000-2zm-9 0a1 1 0 100 2 1 1 0 000-2z" />
@@ -48,7 +48,7 @@
         </div>
 
         <!-- Rendez-vous -->
-        <div class="flex flex-col items-center p-6 bg-white rounded-xl shadow-md hover:shadow-lg transition duration-300 ease-in-out w-full sm:w-1/2 md:w-1/4">
+        <div class="flex flex-col items-center p-6 bg-white ease-in-out w-full sm:w-1/2 md:w-1/4">
             <div class="stat-icon bg-red-100 p-4 rounded-full mb-4">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon h-7 w-7 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
